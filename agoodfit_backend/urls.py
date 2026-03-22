23bad96df6b3f59d2ab9bc003bd408b1da68ca3b
Jonah Onah <onahjonah@gmail.com>
2026-03-22 10:19:38 +0100

    updated api landing

--- agoodfit_backend/urls.py
+++ agoodfit_backend/urls.py
@@ -5,8 +5,10 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
+from django.views.generic import TemplateView
 
 urlpatterns = [
+    path('', TemplateView.as_view(template_name='landing.html'), name='landing'),
     path('admin/', admin.site.urls),
     path('api/users/', include('users.urls')),
     path('api/events/', include('events.urls')),
