9f1b529dbb16212f71d7ac58bc204106e9f95bd4
Jonah Onah <onahjonah@gmail.com>
2026-03-22 09:38:09 +0100

    updated backend settings

--- agoodfit_backend/asgi.py
+++ agoodfit_backend/asgi.py
@@ -2,7 +2,7 @@ import os
 from django.core.asgi import get_asgi_application
 from channels.routing import ProtocolTypeRouter
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'agoodfit_backend.settings.development')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'agoodfit_backend.settings.production')
 
 application = ProtocolTypeRouter({
     "http": get_asgi_application(),
--- agoodfit_backend/wsgi.py
+++ agoodfit_backend/wsgi.py
@@ -1,6 +1,6 @@
 import os
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'agoodfit_backend.settings.development')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'agoodfit_backend.settings.production')
 
 application = get_wsgi_application()
